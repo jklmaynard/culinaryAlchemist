5698d0768c43aa87c16b265f5a11b7b7aca4502d
James Maynard <jameskirkmaynard@gmail.com>
2015-04-07 11:45:41 -0700

    add passing validation specs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,6 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :ingredients
   has_many :notes
+  validates :name, :cooktime, :instructions, :pairings, presence: true
 
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,4 +4,8 @@ describe Recipe do
   it { should have_and_belong_to_many :categories }
   it { should have_and_belong_to_many :ingredients }
   it { should have_many :notes }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cooktime }
+  it { should validate_presence_of :instructions }
+  it { should validate_presence_of :pairings }
 end
