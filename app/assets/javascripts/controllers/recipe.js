6a68370075122a08049b2111220c48369ba8c80f
J. TURNER and Karvari Ellingson <mzphittstudios@gmail.com>
2015-04-09 20:12:27 -0700

    Delete unnecessary controllers; adjust files accordingly.

--- app/assets/javascripts/controllers/recipe.js
+++ app/assets/javascripts/controllers/recipe.js
@@ -1,10 +1,23 @@
-function RecipeCtrl($scope, Session, Recipes) {"use strict";
+function RecipeCtrl($scope, Session, Recipes)
+{"use strict";
+//
+//   $scope.user = Session.requestCurrentUser();
+//   $scope.recipes = Recipes.index();
+//
+//   $scope.logout = function() {
+//     Session.logout();
+//   };
+//
 
-  $scope.user = Session.requestCurrentUser();
-  $scope.recipes = Recipes.index();
-
-  $scope.logout = function() {
-    Session.logout();
-  };
-
+this.recipe.controller('RecipeCtrl', [
+  '$scope', '$location', '$http', function($scope, $location, $http) {
+    $scope.recipes = [];
+    $http.get('./api/recipe.json').success(function(data) {
+      return $scope.recipes = data;
+    });
+    return $scope.viewRecipe = function(id) {
+      return $location.url("/recipes/" + id);
+    };
+  }
+]);
 }
